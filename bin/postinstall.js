5d0e00d0922c951fb8dfcae5bde345f3cd5ff379
Behemouth <Behemouth@users.noreply.github.com>
2015-10-07 15:20:29 +0800

    Fixed:node exec on windows

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 exec = require("child_process").exec;
-nodejs = '"' + process.execPath + '"  ';
+//nodejs = '"' + process.execPath + '"  ';
+nodejs = 'node ';
 exec( nodejs + ' ./node_modules/coffee-script/bin/coffee --compile --bare --no-header .', require_compile);
 
 function require_compile(err) {
   if (err) {
-    console.error(err)
+    console.log(err)
     process.exit(2);
   } else {
     exec(nodejs + ' ./node_modules/light-require-compile/bin/light-require-compile.js ./lib/Client.js ./lib/tpl/static/bundle.js',uglifyjs);
