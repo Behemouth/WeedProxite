41c9834cef64af7c85515788f6d2d7866cbabb8c
Behemouth <Behemouth@users.noreply.github.com>
2015-10-07 15:06:18 +0800

    Fixed:postinstall execPath space on Azure

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 exec = require("child_process").exec;
-
-exec(process.execPath + ' ./node_modules/coffee-script/bin/coffee --compile --bare --no-header .', require_compile);
+nodejs = '"' + process.execPath + '"  ';
+exec( nodejs + ' ./node_modules/coffee-script/bin/coffee --compile --bare --no-header .', require_compile);
 
 function require_compile(err) {
   if (err) {
     console.error(err)
     process.exit(2);
   } else {
-    exec(process.execPath + ' ./node_modules/light-require-compile/bin/light-require-compile.js ./lib/Client.js ./lib/tpl/static/bundle.js',uglifyjs);
+    exec(nodejs + ' ./node_modules/light-require-compile/bin/light-require-compile.js ./lib/Client.js ./lib/tpl/static/bundle.js',uglifyjs);
   }
 }
 
@@ -18,6 +18,6 @@ function uglifyjs(err) {
     console.log(err);
     process.exit(3);
   } else {
-    exec(process.execPath + ' ./node_modules/uglifyjs/bin/uglifyjs ./lib/tpl/static/bundle.js -o ./lib/tpl/static/bundle.min.js --compress --mangle');
+    exec(nodejs + ' ./node_modules/uglifyjs/bin/uglifyjs ./lib/tpl/static/bundle.js -o ./lib/tpl/static/bundle.min.js --compress --mangle');
   }
 }
